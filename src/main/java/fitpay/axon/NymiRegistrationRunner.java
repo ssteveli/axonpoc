d375214a835b0fd7987cc8cc9ee8b149bd592816
Scott Stevelinck <scott@stevelinck.com>
2014-11-17 14:49:59 -0700

    added data validation to the cmds

--- src/main/java/fitpay/axon/NymiRegistrationRunner.java
+++ src/main/java/fitpay/axon/NymiRegistrationRunner.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Random;
 
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.commandhandling.interceptors.JSR303ViolationException;
 import org.axonframework.repository.AggregateNotFoundException;
 import org.axonframework.repository.Repository;
 import org.axonframework.unitofwork.DefaultUnitOfWork;
@@ -26,19 +27,24 @@ public class NymiRegistrationRunner {
                 
         gw = ctx.getBean(CommandGateway.class);
         
-        char[] vkId = new char[]{ '0' };
+        char[] vkId = new char[]{ '0','1','2','3','4','5','6','7','8','9','0','1','2','3','4','5' };
         char[] vkKey = new char[]{ 'a', 'b', 'c' };
         
         Random r = new Random(System.currentTimeMillis());
         List<String> ids = new ArrayList<String>();
         for (int x=0; x<10; x++) {
             CreateNymiRegistrationCommand cmd = new CreateNymiRegistrationCommand(vkId, vkKey);
-            gw.send(cmd);
-            ids.add(cmd.getId());
-            
-            if ((r.nextInt() % 3) == 0) {
-                gw.send(new UpdateNymiRegistrationCommand(cmd.getId(), cmd.getVkId(), new char[]{'x','y','z'}));
+            try {
+                gw.send(cmd);
+                ids.add(cmd.getId());
+                
+                if ((r.nextInt() % 3) == 0) {
+                    gw.send(new UpdateNymiRegistrationCommand(cmd.getId(), cmd.getVkId(), new char[]{'x','y','z'}));
+                }
+            } catch (JSR303ViolationException e) {
+                e.printStackTrace();
             }
+            
         }
         
         @SuppressWarnings("unchecked")
--- src/main/java/fitpay/axon/commands/CreateNymiRegistrationCommand.java
+++ src/main/java/fitpay/axon/commands/CreateNymiRegistrationCommand.java
@@ -2,13 +2,20 @@ package fitpay.axon.commands;
 
 import java.util.UUID;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.axonframework.commandhandling.annotation.TargetAggregateIdentifier;
 
 public class CreateNymiRegistrationCommand {
 
     @TargetAggregateIdentifier
     protected final String id;
+    
+    @NotNull(message = "the vkId is required")
+    @Size(min=16, max=16, message="the vkId must have a size from {min} to {max}")
     private final char[] vkId;
+    
     private final char[] vkKey;
 
     public CreateNymiRegistrationCommand(char[] vkId, char[] vkKey) {
